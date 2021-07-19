866fdfb932ec9f253b3028c82b2ed88b11a7f66f
1901991@sit.singaporetech.edu.sg <1901991@sit.singaporetech.edu.sg>
2021-07-19 16:10:31 +0800

    cleanup

--- app.py
+++ app.py
@@ -2,14 +2,11 @@ import torch
 import pandas
 import cv2
 from os import listdir, remove
-# Model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5m, yolov5x, custom
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # getting the model from torch hub
 
-# Images
-# imglist = ['test1.jpg', 'test2.jpg']
 folder = "./data/"
 imglist = listdir(folder)
-
+deleted = 0
 i = 0
 for v in imglist: # convert first frame in vids to image
     if (".mp4" in v):
@@ -20,14 +17,14 @@ for v in imglist: # convert first frame in vids to image
     i+= 1
 
 imgs = [folder + f for f in imglist]  # batch of images
+
 # Inference
 results = model(imgs)
-# results.crop()
-# results = model(imgs)
+
 for i, (im, pred) in enumerate(zip(results.imgs, results.pred)):
     imgX = im.shape[0]
     imgY = im.shape[1]
-    # print("for " + imglist[i] + ": ")
+    
     people = 0
     largestX = 1
     largestY = 1
@@ -37,12 +34,12 @@ for i, (im, pred) in enumerate(zip(results.imgs, results.pred)):
             people = int(n)
     for *box, conf, cls in reversed(pred):
         if results.names[int(cls)] == "person":
-            #xyxy
             width = int(box[2]) - int(box[0])
             height = int(box[3]) - int(box[1])
             if (width * height) > (largestX * largestY):
                 largestX = width
                 largestY = height
+
     # area comparison vs image size
     dimvsbb = (largestX * largestY) / (imgX * imgY)
     print("dim vs bounding box: " + str(dimvsbb) + " (need 0.1)")
@@ -55,6 +52,7 @@ for i, (im, pred) in enumerate(zip(results.imgs, results.pred)):
                 mp4file = imglist[i].replace(".jpg", "")
                 remove(folder + mp4file)
             print("deleted " + imglist[i])
+            deleted += 1
         except:
             print("failed to delete " + imglist[i])
     elif (".mp4" in imglist[i]): # remove .jpg frame if video passed
@@ -62,3 +60,5 @@ for i, (im, pred) in enumerate(zip(results.imgs, results.pred)):
             remove(folder + imglist[i])
         except:
             pass
+        
+print("deleted " + str(deleted) + " items")
